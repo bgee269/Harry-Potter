3147a8929b2a647a5c5527297accd2c9444f67c0
alinavo <43709347+alinavo@users.noreply.github.com>
2018-12-22 13:35:11 -0500

    Feature/hermione game (#10)
    * created js file
    
    * starting code for game

--- assets/javascript/hermione.js
+++ assets/javascript/hermione.js
@@ -15,10 +15,11 @@ welcome.attr("id", "welcome-message");
 var p = $("<p>");
 p.attr("id", "riddle-text");
 
+
+
 var questions = $("<div>");
 questions.attr("id", "questions-display");
 
-//CONTAINER ONE
 //text of welcome message
 welcome.text("The potions riddle is the last obstacle that Harry and Hermione have to face before entering the chamber where the Sorcerer’s Stone is hidden. Click start to see if you can solve the riddle!")
 containerOne.append(welcome);
@@ -28,15 +29,6 @@ var startBtn = $("<button>");
 startBtn.attr("id", "start-button");
 startBtn.text("Start");
 containerOne.append(startBtn);
-
-//CONTAINER TWO
-//image
-var potionsPic = $("<img>");
-potionsPic.attr("id", "picture");   
-potionsPic.attr('src', 'assets/images/potion.png');
-containerTwo.append(potionsPic);
-console.log("Hello");
-console.log(potionsPic.attr("src"));
 
 //text of riddle
 p.text("Danger lies before you, while safety lies behind, Two of us will help you, whichever you would find, One among us seven will let you move ahead, Another will transport the drinker back instead, Two among our number hold only nettle wine, Three of us are killers, waiting hidden in line. Choose, unless you wish to stay here forevermore, To help you in your choice, we give you these clues four: First, however slyly the poison tries to hide You will always find some on nettle wine's left side; Second, different are those who stand at either end, But if you would move onward, neither is your friend; Third, as you see clearly, all are different size, Neither dwarf nor giant holds death in their insides; Fourth, the second left and the second on the right Are twins once you taste them, though different at first sight.");
