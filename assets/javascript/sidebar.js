8065b145f5f58329f6f58c4082d0c6a32bbb535b
Thomas Woods <>
2018-12-23 11:52:27 -0500

    mouse hover changes sidebar text

---
+++ assets/javascript/sidebar.js
@@ -0,0 +1,34 @@
+$("#patronus-quiz-nav").on("mouseover", function(event) {
+    $("#patronus-quiz-nav").text("Take a nine question quiz to find your inner Patronus");
+});
+$("#patronus-quiz-nav").on("mouseout", function(event) {
+    $("#patronus-quiz-nav").text("Defense Against the Dark Arts");
+});
+
+$("#chat-room-nav").on("mouseover", function(event) {
+    $("#chat-room-nav").text("Chat with fellow Potter fans in a fun and welcoming place");
+});
+$("#chat-room-nav").on("mouseout", function(event) {
+    $("#chat-room-nav").text("Great Hall");
+});
+
+$("#movie-section-nav").on("mouseover", function(event) {
+    $("#movie-section-nav").text("Learn about the wizarding world of Harry Potter films");
+});
+$("#movie-section-nav").on("mouseout", function(event) {
+    $("#movie-section-nav").text("A History of Magic");
+});
+
+$("#house-points-nav").on("mouseover", function(event) {
+    $("#house-points-nav").text("Check in on the current house point standings");
+});
+$("#house-points-nav").on("mouseout", function(event) {
+    $("#house-points-nav").text("House Points");
+});
+
+$("#sorting-quiz-nav").on("mouseover", function(event) {
+    $("#sorting-quiz-nav").text("A short quiz will place you in one of the four houses");
+});
+$("#sorting-quiz-nav").on("mouseout", function(event) {
+    $("#sorting-quiz-nav").text("Sorting Ceremony");
+});
