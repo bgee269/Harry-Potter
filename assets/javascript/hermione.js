b8625f4d989f83a73ba8501618366e44d6e0d442
Thomas Woods <>
2019-01-08 20:32:36 -0500

    fixed game

--- assets/javascript/hermione.js
+++ assets/javascript/hermione.js
@@ -31,10 +31,11 @@ containerOne.append(startBtn);
 //image
 var potionsPic = $("<img>");
 potionsPic.attr("id", "picture");   
-potionsPic.attr('src', 'assets/images/potion.png');
+potionsPic.attr('src', 'assets/images/potions.png');
 containerTwo.append(potionsPic);
 console.log("Hello");
 console.log(potionsPic.attr("src"));
+console.log(containerTwo);
 
 //text of riddle
 p.text("Danger lies before you, while safety lies behind, Two of us will help you, whichever you would find, One among us seven will let you move ahead, Another will transport the drinker back instead, Two among our number hold only nettle wine, Three of us are killers, waiting hidden in line. Choose, unless you wish to stay here forevermore, To help you in your choice, we give you these clues four: First, however slyly the poison tries to hide You will always find some on nettle wine's left side; Second, different are those who stand at either end, But if you would move onward, neither is your friend; Third, as you see clearly, all are different size, Neither dwarf nor giant holds death in their insides; Fourth, the second left and the second on the right Are twins once you taste them, though different at first sight.");
@@ -49,19 +50,21 @@ containerTwo.append(riddleQuestion);
 var answerArray = [1, 2, 3, 4, 5, 6, 7];
 for (var i = 0; i < answerArray.length; i++) {
     var button = $("<button>").text(answerArray[i]);
+    button.attr("index", i);
+    button.addClass("answerButton");
     containerTwo.append(button);
 };
 
 var userSelect;
 
 //Function that answers the question
-function answerQuestion() {
+function answerQuestion(theObject) {
 
-    $(button).on('click', function(){
-        userSelect = $(this).data('index');
-    }); 
-
-    if (userSelect === 3) {
+    
+    console.log(theObject.text());
+    userSelect = theObject.text();
+    console.log(userSelect);
+    if (userSelect === "3") {
         //question answered correctly
         $("#main-content-area").empty();
         $("#main-content-area").append("That is correct!");
@@ -79,7 +82,6 @@ function answerQuestion() {
 //CHANGE TO ONCLICK WITH HTML CLICK
 $("#hermione-game-nav").on("click", function (event) {
     event.preventDefault();
-
     $("#main-content-area").empty();
     $("#main-content-area").append(containerOne);
 
@@ -88,11 +90,10 @@ $("#hermione-game-nav").on("click", function (event) {
 $(document).on("click", "#start-button", function () {
     $("#main-content-area").empty();
     $("#main-content-area").append(containerTwo);
-    answerQuestion();
+    // answerQuestion();
 
 });
 
-
-
-
-
+$(document).on("click", ".answerButton", function () {
+    answerQuestion($(this));
+});
